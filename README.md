7397ccacad21af6cdafdbcb3ef44361918c16a83
MidCoard <2624646185@qq.com>
2022-04-22 10:24:50 +0800

    update to 1.2.8 add UnmodifiableView annotations

--- README.md
+++ README.md
@@ -10,12 +10,12 @@ Maven:
 <dependency>
     <groupId>top.focess</groupId>
     <artifactId>focess-command</artifactId>
-    <version>1.2.7</version>
+    <version>1.2.8</version>
 </dependency>
 ```
 
 Gradle:
 
 ```gradle
-implementation 'top.focess:focess-command:1.2.7'
+implementation 'top.focess:focess-command:1.2.8'
 ```
--- src/main/java/top/focess/command/Command.java
+++ src/main/java/top/focess/command/Command.java
@@ -4,8 +4,10 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.UnmodifiableView;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Predicate;
@@ -78,8 +80,9 @@ public abstract class Command {
      * @return All commands as a list
      */
     @NotNull
+    @UnmodifiableView
     public static List<Command> getCommands() {
-        return Lists.newArrayList(COMMANDS_MAP.values());
+        return Collections.unmodifiableList(Lists.newArrayList(COMMANDS_MAP.values()));
     }
 
     /**
